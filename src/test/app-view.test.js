84cae24595ce3c8749adb528828e2f4b20b1f41b
Jérémie Zarca <jeremie.zarca@gmail.com>
2018-01-31 12:31:57 +0100

    updated tests

--- src/test/app-view.test.js
+++ src/test/app-view.test.js
@@ -1,15 +1,8 @@
 // Link.react.test.js
 import React from 'react';
 import AppView from '../components/app-view';
-import renderer from 'react-test-renderer';
-import {
-    StaticRouter
-} from 'react-router-dom';
+import {shallow} from 'enzyme';
 
 it('renders without even crashing', () => {
-  const component = renderer.create(
-    <AppView />
-  );
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+  shallow(<AppView />);
 });
